fefeda9e94f7d72faff754f86e0595d2e45573e7
morgantstewart <morgan@paysigil.com>
2025-09-05 22:45:30 -0400

    Clues #2 and #3: Solved

--- clues.sql
+++ clues.sql
@@ -1,17 +1,28 @@
--- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+-- -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
--- Write SQL query here
-SELECT name FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC;
+-- -- Write SQL query here
+-- SELECT name FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC;
 
--- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+-- -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
--- Write SQL query here
-
+-- -- Write SQL query here
+-- SELECT cl.language 
+-- FROM countrylanguages cl 
+-- JOIN countries c ON cl.countrycode = c.code 
+-- WHERE c.name = 'Holy See (Vatican City State)' AND cl.isofficial = true;
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
-
+SELECT c.name 
+FROM countries c 
+JOIN countrylanguages cl ON c.code = cl.countrycode 
+WHERE cl.language = 'Italian' 
+AND c.code NOT IN (
+    SELECT countrycode 
+    FROM countrylanguages 
+    WHERE language != 'Italian'
+);
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
