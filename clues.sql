b4e27a3b1003668469711324a6d23e7f6d514980
morgantstewart <morgan@paysigil.com>
2025-09-05 22:53:47 -0400

    Clue #5Solved

--- clues.sql
+++ clues.sql
@@ -27,11 +27,20 @@ AND c.code NOT IN (
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
+SELECT name 
+FROM cities 
+WHERE countrycode = 'SMR' 
+AND name != 'San Marino';
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+SELECT c.name, co.name as country
+FROM cities c
+JOIN countries co ON c.countrycode = co.code
+WHERE co.continent = 'South America'
+AND c.name LIKE 'Serra%';
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -39,7 +48,8 @@ AND c.code NOT IN (
 -- follow right behind you!
 
 -- Write SQL query here
-
+FIND capital
+OF 'Brazil';
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
